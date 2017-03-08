ce6dda7679f89cea414153e129f3b3ebce40e7d1
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2017-03-08 00:16:09 -0500

    First commit. Engine architecture skeleton laid out.

---
+++ Lania Engine/Kernel/LaniaEngine.cpp
@@ -0,0 +1,47 @@
+/*
+**						This file is part of:
+**							LANIA ENGINE
+**
+** Copyright(c) 2017 Jean-Louis Haywood
+**
+** Permission is hereby granted, free of charge, to any person obtaining a copy
+** of this software and associated documentation files(the "Software"), to deal
+** in the Software without restriction, including without limitation the rights
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+** copies of the Software, and to permit persons to whom the Software is
+** furnished to do so, subject to the following conditions :
+**
+** The above copyright notice and this permission notice shall be included in all
+** copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+** SOFTWARE.
+*/
+
+/**
+* Lania Engine
+*
+* Description: Lania Engine, named after the Laniakea Supercluster (Hawaiian for
+* "immense heaven"), is a 2D & 3D game engine that is being developed for personal
+* research, learning & fun.
+*
+* Author:         Jean-Louis Haywood
+* Created:        5th March 2017
+*/
+
+#include "LaniaEngine.hpp"
+#include "Initialization.hpp"
+#include "SimulationLoop.hpp"
+#include "Shutdown.hpp"
+
+void run_LaniaEngine()
+{
+	initializeEngine();
+	runSimulationLoop();
+	shutdownEngine();
+}
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# LaniaEngine
-Lania Engine – 2D and 3D Game Engine. For Personal Research, Learning &amp; Fun.
+# Lania Engine
+
+Lania Engine, named after the Laniakea Supercluster (Hawaiian for "immense heaven"), is a 2D & 3D game engine that is being developed for personal research, learning & fun.
+
+# Third Party Libraries
+
+## SDL
+
+- Upstream: https://www.libsdl.org/
+- Version: 2.0.5
+- License: zlib
+
+## OpenAL Soft
+
+- Upstream: http://kcat.strangesoft.net/openal.html
+- Version: 1.17.2
+- License: LGPL-licensed
+
+
+## libpng
+
+- Upstream: http://libpng.org/pub/png/libpng.html
+- Version: 1.6.28
+- License: libpng/zlib
+
+
+## GLEW
+
+- Upstream: http://glew.sourceforge.net/
+- Version: 2.0.0
+- License: MIT, BSD
+
+## GLM 
+
+- Upstream: http://glm.g-truc.net/0.9.8/index.html
+- Version: 0.9.8.4
+- License: MIT
