ba48687c42e55e7f6d04094d691d346b25548f05
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2017-03-13 16:12:17 -0400

    Added Events class for the eventSystem object needed to detect OS messages and assign the inputSystem buffers the detected key symbols (keyboard only for now).

---
+++ Lania Engine/Modules/Common/Events/Events.cpp
@@ -0,0 +1,55 @@
+/*
+**                     This file is part of:
+**                         LANIA ENGINE
+**
+** Copyright(c) 2017 Jean-Louis Haywood
+**
+** Permission is hereby granted, free of charge, to any person obtaining a copy
+** of this software and associated documentation files(the "Software"), to deal
+** in the Software without restriction, including without limitation the rights
+** to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+** copies of the Software, and to permit persons to whom the Software is
+** furnished to do so, subject to the following conditions :
+**
+** The above copyright notice and this permission notice shall be included in all
+** copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+** SOFTWARE.
+*/
+
+#include "Events.hpp"
+
+bool Events::handleSDLEvents(Input* inputSystem)
+{
+	while (SDL_PollEvent(&SDLEvents))
+	{
+		switch (SDLEvents.type)
+		{
+			/*Keyboard inputs*/
+			case SDL_KEYDOWN:
+				if (SDLEvents.key.keysym.sym == SDLK_ESCAPE)
+				{
+					return false;
+				}
+				else if (SDLEvents.key.keysym.sym < 128)
+				{
+					inputSystem->keyboardBuffer[SDLEvents.key.keysym.sym] = true;
+				}
+				break;
+
+			case SDL_KEYUP: if (SDLEvents.key.keysym.sym < 128)
+							{
+								inputSystem->keyboardBuffer[SDLEvents.key.keysym.sym] = false;
+							} 
+			break;
+		}
+	}
+
+	return true;
+}
