ff2ce996b4db33068db869d69367ec7198693fd7
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2017-11-13 23:19:33 -0500

    Established a GL context for the engine

--- LaniaEngine/Core/main.cpp
+++ LaniaEngine/Core/main.cpp
@@ -11,6 +11,7 @@
 */
 
 #include <SDL.h>
+#include <GL/glew.h>
 #include "Timing.hpp"
 #include "Logging.hpp"
 #include "Events.hpp"
@@ -19,7 +20,12 @@
 
 int main()
 {
+	SDL_Window *window;
+	SDL_GLContext context;
 	Lania::Timing timing = {0.0};
+
+	int windowWidth = 800;
+	int windowHeight = 600;
 
 	if (SDL_Init(SDL_INIT_EVERYTHING))
 	{
@@ -28,7 +34,31 @@ int main()
 	}
 	else
 	{
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
+		window = SDL_CreateWindow(
+			"Lania Engine",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			windowWidth,
+			windowHeight,
+			SDL_WINDOW_OPENGL);
+
+		if (window == NULL)
+		{
+			SDL_Log(
+				"SDL could not create the window because: %s",
+				SDL_GetError());
+			return 1;
+		}
+
+		context = SDL_GL_CreateContext(window);
+		SDL_GL_SetSwapInterval(1);
+
+		glViewport(0, 0, windowWidth, windowHeight);
 	}
 
 	while(handleEvents())
