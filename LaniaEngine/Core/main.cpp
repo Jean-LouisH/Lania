91a760cc285a889cf1e44170331d1b0527ec11d6
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2017-11-14 21:42:26 -0500

    Separated initialization and shutdown, set the SDL swap interval to 0.

--- LaniaEngine/Core/main.cpp
+++ LaniaEngine/Core/main.cpp
@@ -12,25 +12,56 @@
 
 #include <SDL.h>
 #include <GL/glew.h>
+
 #include "Timing.hpp"
 #include "Logging.hpp"
 #include "Events.hpp"
 
 #undef main main //reverses SDL's main definition.
 
+namespace Lania
+{
+	bool initialize(
+		SDL_Window* window,
+		SDL_GLContext* context);
+	void shutdown(
+		SDL_Window* window,
+		SDL_GLContext context);
+}
+
 int main()
 {
-	SDL_Window *window;
+	SDL_Window *window = NULL;
 	SDL_GLContext context;
 	Lania::Timing timing = {0.0};
 
+	Lania::initialize(window, &context);
+
+	while(handleEvents())
+	{
+		timing.current = SDL_GetTicks();
+		timing.simulation += (timing.current - timing.last);
+		timing.last = timing.current;
+		SDL_Delay(1000 / 60);
+	}
+
+	Lania::shutdown(window, context);
+	return 0;
+}
+
+bool Lania::initialize(
+	SDL_Window* window,
+	SDL_GLContext* context)
+{
 	int windowWidth = 800;
 	int windowHeight = 600;
 
 	if (SDL_Init(SDL_INIT_EVERYTHING))
 	{
-		SDL_Log("SDL could not initialize because: %s", SDL_GetError);
-		return 1;
+		SDL_Log(
+			"SDL could not initialize because: %s", 
+			SDL_GetError);
+		return false;
 	}
 	else
 	{
@@ -52,24 +83,21 @@ int main()
 			SDL_Log(
 				"SDL could not create the window because: %s",
 				SDL_GetError());
-			return 1;
+			return false;
 		}
 
-		context = SDL_GL_CreateContext(window);
-		SDL_GL_SetSwapInterval(1);
-
+		*context = SDL_GL_CreateContext(window);
+		SDL_GL_SetSwapInterval(0);
 		glViewport(0, 0, windowWidth, windowHeight);
+		return true;
 	}
+}
 
-	while(handleEvents())
-	{
-		timing.current = SDL_GetTicks();
-		timing.simulation += (timing.current - timing.last);
-		timing.last = timing.current;
-		SDL_Delay(1000 / 60);
-	}
-
+void Lania::shutdown(
+	SDL_Window* window,
+	SDL_GLContext context)
+{
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
-
-	return 0;
 }
