f479113314aaaf16deaedfe385fb0f68d558a7cb
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2017-11-24 17:33:57 -0500

    Initializes attributes and libraries based on API selection: OpenGL or Vulkan

--- LaniaEngine/Core/LaniaEngine.cpp
+++ LaniaEngine/Core/LaniaEngine.cpp
@@ -6,12 +6,14 @@
 
 Lania::EngineData Lania::initialize()
 {
-
 	//Development Test 
 	std::string initFilePath = "../Demos/Basic/Init.cfg";
 
 	EngineData engine = Config::parseInit(File::read(initFilePath));
 	engine.state = RUNNING;
+	engine.APIVersion = NULL;
+	engine.glContext = NULL;
+	engine.platform = (char*)SDL_GetPlatform();
 
 	if (SDL_Init(SDL_INIT_EVERYTHING))
 	{
@@ -22,17 +24,22 @@ Lania::EngineData Lania::initialize()
 	}
 	else
 	{
-		engine.targetFPS = 60;
-
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-		SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-		SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
-		SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
-		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		if (engine.windowFlags & SDL_WINDOW_OPENGL)
+		{
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+			SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+			SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+			SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+			SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+			SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+			SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+			SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+			SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+		}
+		else if (engine.windowFlags & SDL_WINDOW_VULKAN)
+		{
+			;
+		}
 
 		engine.window = SDL_CreateWindow(
 			engine.windowTitle.c_str(),
@@ -50,15 +57,22 @@ Lania::EngineData Lania::initialize()
 			engine.state = SHUTDOWN;
 		}
 
-		engine.glContext = SDL_GL_CreateContext(engine.window);
-		glViewport(0, 0, engine.windowWidthPixels, engine.windowHeightPixels);
-
-		if (glewInit() != GLEW_OK)
+		if (engine.windowFlags & SDL_WINDOW_OPENGL)
 		{
-			engine.state = SHUTDOWN;
-		}
+			engine.glContext = SDL_GL_CreateContext(engine.window);
+			glViewport(0, 0, engine.windowWidthPixels, engine.windowHeightPixels);
 
-		engine.APIVersion = (char*)glGetString(GL_VERSION);
+			if (glewInit() != GLEW_OK)
+			{
+				engine.state = SHUTDOWN;
+			}
+			engine.APIVersion = (char*)glGetString(GL_VERSION);
+		}
+		else if (engine.windowFlags & SDL_WINDOW_VULKAN)
+		{
+			;
+		}
+		engine.targetFPS = 60;
 	}
 	return engine;
 }
