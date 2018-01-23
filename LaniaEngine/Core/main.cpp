e0a8ba1b13ba6b92d05de833d026360409ea7266
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-01-22 23:52:48 -0500

    Added early implementation of a fixed update loop with optional frame-rate locking

--- LaniaEngine/Core/main.cpp
+++ LaniaEngine/Core/main.cpp
@@ -14,7 +14,7 @@
 #include <vector>
 
 #include "LaniaEngine.hpp"
-#include "EngineData.hpp"
+#include "Engine.hpp"
 #include "Timing.hpp"
 #include "UnitTesting.hpp"
 #include "OS/File.hpp"
@@ -26,8 +26,10 @@
 
 int main(int argc, char* argv[])
 {
-	Lania::Timing time = {0.0};
-	Lania::EngineData engine = Lania::initialize();
+	const int MS_PER_UPDATE = 8;
+
+	Lania::Timing time;
+	Lania::Engine engine = Lania::init();
 
 	char* mainSceneContents = File::read(
 		"../Demos/Basic/" +
@@ -48,6 +50,12 @@ int main(int argc, char* argv[])
 		OS::handleEvents(&engine);
 
 		time.simulation += time.frame;
+		time.lag += time.frame;
+
+		while (time.lag >= MS_PER_UPDATE)
+		{
+			time.lag -= MS_PER_UPDATE;
+		}
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		SDL_GL_SwapWindow(engine.window);
@@ -56,11 +64,10 @@ int main(int argc, char* argv[])
 		time.cycleEnd = SDL_GetTicks();
 		time.cycleDelta = time.cycleEnd - time.cycleStart;
 		time.frameDelay = (1000.0 / engine.targetFPS) - time.cycleDelta;
-		if (time.frameDelay > 0)
+		if (time.frameDelay > 0 && engine.targetFPS != -1)
 			SDL_Delay(time.frameDelay);
 		time.frame = SDL_GetTicks() - time.cycleStart;
 	}
-
 	Lania::shutdown(&engine);
 	return 0;
 }
