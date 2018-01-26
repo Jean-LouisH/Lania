d77d50395639df277dc74565c03e5a643e036d4a
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-01-26 00:34:33 -0500

    Added Application, Input and Output structs to work with the Engine struct

---
+++ LaniaEngine/Core/Input.hpp
@@ -0,0 +1,20 @@
+/**
+*                      This file is part of:
+*                          Lania Engine
+*           https://jean-louish.github.io/LaniaEngine/
+*
+* Input
+*
+*
+*
+* Copyright (c) 2017-2018 Jean-Louis Haywood. All rights reserved.
+*/
+
+#pragma once
+
+#include "../Input/Keyboard.hpp"
+
+typedef struct Input
+{
+
+}Input;
---
+++ LaniaEngine/Core/Output.hpp
@@ -0,0 +1,18 @@
+/**
+*                      This file is part of:
+*                          Lania Engine
+*           https://jean-louish.github.io/LaniaEngine/
+*
+* Output
+*
+*
+*
+* Copyright (c) 2017-2018 Jean-Louis Haywood. All rights reserved.
+*/
+
+#pragma once
+
+typedef struct Output
+{
+
+}Output;
