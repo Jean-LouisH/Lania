c180383132586a3c1d52f7c4c697a863dc7be348
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-01-27 13:10:57 -0500

    Added a header specifically for constants

---
+++ LaniaEngine/Constants.hpp
@@ -0,0 +1,15 @@
+/**
+*                      This file is part of:
+*                          Lania Engine
+*           https://jean-louish.github.io/LaniaEngine/
+*
+* Constants
+*
+*
+*
+* Copyright (c) 2017-2018 Jean-Louis Haywood. All rights reserved.
+*/
+
+#define MS_PER_UPDATE 8
+#define S_PER_NS 1000000000
+#define MS_PER_NS 1000000
