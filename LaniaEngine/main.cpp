93e44d65bcb55229b6a94d603c94015ec90f1d38
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-06-21 23:32:38 -0400

    dynamically allocates engine and application due to size on stack as fields are more defined.

--- LaniaEngine/main.cpp
+++ LaniaEngine/main.cpp
@@ -14,13 +14,17 @@
 
 int main(int argc, char* argv[])
 {
-	Lania::Engine engine;
-	Lania::Application application;
+	Lania::Engine* engine = new Lania::Engine();
+	Lania::Application* application = new Lania::Application();
 	
-	engine.filepath = argv[0];
-	Lania::initialize(&engine);
-	if (engine.state == Lania::RUNNING_APPLICATION)
-		Lania::loop(&engine, &application);
-	Lania::shutdown(&engine, &application);
+	engine->filepath = argv[0];
+	Lania::initialize(engine);
+	if (engine->state == Lania::RUNNING_APPLICATION)
+		Lania::loop(engine, application);
+	Lania::shutdown(engine, application);
+
+	delete application;
+	delete engine;
+
 	return 0;
 }
