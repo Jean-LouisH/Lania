4c1df9aab00519f14329a004aa9a0e91dcfce8de
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-06-21 23:31:52 -0400

    Added Stack data structure

---
+++ LaniaEngine/Core/DataStructures/Stack.hpp
@@ -0,0 +1,19 @@
+/**
+*                      This file is part of:
+*                          Lania Engine
+*           https://jean-louish.github.io/LaniaEngine/
+*
+* Stack
+*
+* Copyright (c) 2017 Jean-Louis Haywood. All rights reserved.
+*/
+
+#pragma once
+
+namespace Lania
+{
+	typedef struct Stack
+	{
+
+	}Stack;
+}
