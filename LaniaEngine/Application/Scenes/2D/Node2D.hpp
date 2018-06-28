b1377ed59c80bd604d49198b3d0630f849cfaca3
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-06-27 21:38:59 -0400

    Specified Nodes as 2D

--- LaniaEngine/Application/Scenes/2D/Node2D.hpp
+++ LaniaEngine/Application/Scenes/2D/Node2D.hpp
@@ -3,9 +3,9 @@
 *                          Lania Engine
 *           https://jean-louish.github.io/LaniaEngine/
 *
-* Node
+* Node2D
 *
-* Unit of data points in the scene graph
+* Unit of data points in the 2D scene graph
 *
 * Copyright (c) 2017-2018 Jean-Louis Haywood. All rights reserved.
 */
@@ -13,14 +13,16 @@
 #pragma once
 
 #include <Core/DataStructures/List.hpp>
+#include <Application/Scenes/2D/Vector2.hpp>
 #include <stdint.h>
 
 namespace Lania
 {
-	typedef struct Node
+	typedef struct Node2D
 	{
 		uint64_t id; //Memory address of the struct composed of this node.
-		Node* parent;
-		List<Node*> children;
-	}Node;
+		Node2D* parent;
+		List<Node2D*> children;
+		Vector2 position;
+	}Node2D;
 }
