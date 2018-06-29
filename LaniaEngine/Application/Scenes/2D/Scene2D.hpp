f42258bdb0ab6051d8e96dbe6624d6f30acc34f2
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-06-29 10:06:45 -0400

    Defined full component lists for the 2D scene

--- LaniaEngine/Application/Scenes/2D/Scene2D.hpp
+++ LaniaEngine/Application/Scenes/2D/Scene2D.hpp
@@ -13,11 +13,21 @@
 #include <map>
 #include <stdint.h>
 #include <Core/Input.hpp>
+#include <Core/DataStructures/List.hpp>
+#include "Camera2D.hpp"
+#include "Entity2D.hpp"
+#include "RigidBody2D.hpp"
+#include "Sprite.hpp"
+#include "StaticBody2D.hpp"
 
 namespace Lania
 {
 	typedef struct Scene2D
 	{
-
+		List<Camera2D> cameras;
+		List<Entity2D> entities;
+		List<RigidBody2D> rigidBodies;
+		List<Sprite> sprites;
+		List<StaticBody2D> staticBodies;
 	}Scene2D;
 }
