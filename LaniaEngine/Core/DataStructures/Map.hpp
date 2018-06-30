ec2f59e070a5363bb2d09cc8e36366c7d8a8d207
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-06-30 01:42:39 -0400

    Combined multimap and pair allocators into the map header.

--- LaniaEngine/Core/DataStructures/Map.hpp
+++ LaniaEngine/Core/DataStructures/Map.hpp
@@ -15,5 +15,11 @@
 namespace Lania
 {
 	template <class _Kty, class _Ty>
+	using Pair = std::pair<_Kty, _Ty>;
+
+	template <class _Kty, class _Ty>
 	using Map = std::map<const _Kty, _Ty>;
+
+	template <class _Kty, class _Ty>
+	using Multimap = std::multimap<const _Kty, _Ty>;
 }
