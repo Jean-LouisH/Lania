dbae5cb4f6f80a05ab6007494a075cc60f1b0e49
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-06-30 01:45:18 -0400

    Initializes filepath through SDL.

--- LaniaEngine/main.cpp
+++ LaniaEngine/main.cpp
@@ -17,7 +17,6 @@ int main(int argc, char* argv[])
 	Lania::Engine* engine = new Lania::Engine();
 	Lania::Application* application = new Lania::Application();
 	
-	engine->filepath = argv[0];
 	Lania::initialize(engine);
 	if (engine->state == Lania::RUNNING_APPLICATION)
 		Lania::loop(engine, application);
