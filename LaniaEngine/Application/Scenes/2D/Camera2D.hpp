ceee95f539c1c5fca8af0fcea3461e0fa806c47f
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-06-30 01:49:11 -0400

    Provided full data set for camera2D operations

--- LaniaEngine/Application/Scenes/2D/Camera2D.hpp
+++ LaniaEngine/Application/Scenes/2D/Camera2D.hpp
@@ -10,15 +10,38 @@
 
 #pragma once
 
+#include <stdint.h>
+#include <Application/Scenes/2D/AABB2D.hpp>
 #include <Application/Scenes/2D/Rect.hpp>
 #include <Application/Scenes/2D/Vector2.hpp>
+#include <Application/Scenes/2D/Entity2D.hpp>
 
 namespace Lania
 {
 	typedef struct Camera2D
 	{
-		Rect viewport;
-		double rotation;
+		Entity2D* container;
+		Rect viewport_px;
+		uint64_t entityFollowed_id;
+		Vector2 scrollTarget_px;
+		Vector2 offset_px;
+		Rect zoomTarget_px;
+		AABB2D limits;
+		double scrollTargetSpeed_px_per_s;
+		double zoomTargetSpeed_px_per_s;
+		bool isRotatingWithEntity;
+		bool followX;
+		bool followY;
+		bool current;
+
+		Camera2D() :
+			scrollTargetSpeed_px_per_s(0.0),
+			zoomTargetSpeed_px_per_s(0.0),
+			isRotatingWithEntity(false),
+			followX(false),
+			followY(false),
+			current(false)
+		{}
 	}Camera2D;
 
 }
