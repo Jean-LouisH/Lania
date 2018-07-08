3031c93fd85e4c381c75d5d4a8268fada56f5845
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-07-08 12:03:56 -0400

    Separates components into active and inactive lists, and stores the current camera

--- LaniaEngine/Application/Scenes/2D/Scene2D.hpp
+++ LaniaEngine/Application/Scenes/2D/Scene2D.hpp
@@ -14,20 +14,38 @@
 #include <stdint.h>
 #include <Core/Input.hpp>
 #include <Core/DataStructures/List.hpp>
+#include <Core/DataStructures/Map.hpp>
 #include "Camera2D.hpp"
 #include "Entity2D.hpp"
 #include "RigidBody2D.hpp"
 #include "Sprite.hpp"
 #include "StaticBody2D.hpp"
 
+using ComponentListIndex = uint64_t;
+
 namespace Lania
 {
 	typedef struct Scene2D
 	{
-		List<Camera2D> cameras;
+		//Entities listed and layered by ID numbers
 		List<Entity2D> entities;
-		List<RigidBody2D> rigidBodies;
-		List<Sprite> sprites;
-		List<StaticBody2D> staticBodies;
+
+		//Active component existence-based processing lists
+		//Ordered by entity layer, but not necessarily matched to IDs
+		//Components contain parent entity IDs
+		List<Sprite> activeSprites;
+		List<Camera2D> activeCameras;
+		List<RigidBody2D> activeRigidBodies;
+		List<StaticBody2D> activeStaticBodies;
+
+		//Inactive storage lists
+		//No ordering enforced as long as components maintain parent entity IDs
+		List<Sprite> inactiveSprites;
+		List<Camera2D> inactiveCameras;
+		List<RigidBody2D> inactiveRigidBodies;
+		List<StaticBody2D> inactiveStaticBodies;
+
+		//Stores current camera list position to prevent repetitive searches
+		ComponentListIndex currentCameraIndex;
 	}Scene2D;
 }
