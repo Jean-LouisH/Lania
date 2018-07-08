c7cebce99a6fe242088ce3dbb2b534d7e9041d1d
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-07-08 12:03:10 -0400

    Gave entity2D child and parent IDs, flags for attached components

--- LaniaEngine/Application/Scenes/2D/Entity2D.hpp
+++ LaniaEngine/Application/Scenes/2D/Entity2D.hpp
@@ -13,32 +13,38 @@
 #pragma once
 
 #include <stdint.h>
+#include <limits.h>
 #include <Application/Scenes/2D/Transform2D.hpp>
 #include <Core/DataStructures/Map.hpp>
+#include <Application/Scenes/2D/Camera2D.hpp>
+#include <Application/Scenes/2D/RigidBody2D.hpp>
+#include <Application/Scenes/2D/Sprite.hpp>
+#include <Application/Scenes/2D/StaticBody2D.hpp>
+
+using EntityID = uint64_t;
 
 namespace Lania
 {
-	enum components2D
+	enum Components
 	{
-		CAMERA2D,
-		RIGIDBODY2D,
-		POSITION,
-		SPRITE,
-		STATICBODY2D,
-		VELOCITY,
+		NONE			= 0,
+		CAMERA2D		= 1,
+		RIGIDBODY2D		= 1 << 2,
+		SPRITE			= 1 << 2,
+		STATICBODY2D	= 1 << 3
 	};
 
 	typedef struct Entity2D
 	{
-		uint64_t id;
 		Transform2D transform;
-		Map<uint8_t, uint64_t> components;
-		Map<uint64_t, Entity2D*> children;
-		Entity2D* parent;
+		List<EntityID> children;
+		EntityID parent;
+		uint32_t attachedComponentsFlag; //to determine if searches are necessary
 
-		Entity2D():
-			id(0),
-			parent(NULL)
+		Entity2D() :
+			//Assigned to largest value to represent no parent.
+			parent(ULLONG_MAX),
+			attachedComponentsFlag(0)
 		{}
 	}Entity2D;
 }
