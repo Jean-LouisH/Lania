f5c21b8530698abb5e0fc6acd8610ba50c2c0811
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-07-08 11:56:49 -0400

    Test: added apple entity to attach to magnemite

--- LaniaEngine/Core/LaniaEngine.cpp
+++ LaniaEngine/Core/LaniaEngine.cpp
@@ -41,8 +41,8 @@ void Lania::initialize(Engine* engine)
 	}
 	else
 	{
+		//Development test
 		appConfig->targetFPS = 60;
-
 		IMG_Init(IMG_INIT_PNG);
 
 		if (appConfig->windowFlags & SDL_WINDOW_OPENGL)
@@ -79,7 +79,13 @@ void Lania::initialize(Engine* engine)
 		}
 		else
 		{
-			String iconString = "../Demos/" + appConfig->appName + "/Icon.png";
+			String rootPath = "";
+
+#if _DEBUG
+			//Development test
+			rootPath += "../Demos/";
+#endif
+			String iconString = rootPath + appConfig->appName + "/Icon.png";
 			SDL_Surface* logo = IMG_Load(iconString.c_str());
 			SDL_SetWindowIcon(engine->window, logo);
 			SDL_FreeSurface(logo);
@@ -97,10 +103,13 @@ void Lania::initialize(Engine* engine)
 				{
 					*state = SHUTDOWN;
 				}
-				String* renderingAPIString = new String;
-				*renderingAPIString += "OpenGL ";
-				*renderingAPIString += (char*)glGetString(GL_VERSION);
-				engine->platform.renderingAPIVersion = (char*)renderingAPIString->c_str();
+				else
+				{
+					String* renderingAPIString = new String;
+					*renderingAPIString += "OpenGL ";
+					*renderingAPIString += (char*)glGetString(GL_VERSION);
+					engine->platform.renderingAPIVersion = (char*)renderingAPIString->c_str();
+				}
 			}
 			else if (appConfig->windowFlags & SDL_WINDOW_VULKAN)
 			{
@@ -126,55 +135,92 @@ void Lania::loop(Engine* engine, Application* application)
 	Timer* time = &engine->timer;
 	time->FPS.setStart();
 
+	///////// Temporary Testing (Before Procedures): Entity Component System Loading Algorithms
+
 	Scene2D scene2D;
-	Entity2D cameraEntity;
-	Camera2D camera;
-	cameraEntity.transform.position_px.x = engine->appConfig.windowWidth_px / 2;
-	cameraEntity.transform.position_px.y = engine->appConfig.windowHeight_px / 2;
-	camera.viewport_px.width = engine->appConfig.windowWidth_px;
-	camera.viewport_px.height = engine->appConfig.windowHeight_px;
-	camera.current = true;
+
+	Entity2D camera;
+	//Assign data, centred to screen
+	camera.transform.position_px.x = engine->appConfig.windowWidth_px / 2;
+	camera.transform.position_px.y = engine->appConfig.windowHeight_px / 2;
+	//Add to scene
+	scene2D.entities.push_back(camera);
+
+	Camera2D camera2D;
+	//Assign data, viewing the entire scene
+	camera2D.viewport_px.width = engine->appConfig.windowWidth_px;
+	camera2D.viewport_px.height = engine->appConfig.windowHeight_px;
+	camera2D.current = true;
+	//Attach to last entity
+	camera2D.entityID = scene2D.entities.size() - 1;
+	scene2D.entities.back().attachedComponentsFlag |= CAMERA2D;
+	//Add to scene
+	scene2D.activeCameras.push_back(camera2D);
+	//Camera assigned as current
+	scene2D.currentCameraIndex = scene2D.activeCameras.size() - 1;
+
+	//Once again
+
 	Entity2D magnemite;
 	magnemite.transform.position_px.x = engine->appConfig.windowWidth_px / 2;
 	magnemite.transform.position_px.y = engine->appConfig.windowHeight_px / 2;
-	cameraEntity.id = 1;
-	magnemite.id = 2;
-	Sprite sprite;
-	SDL_Surface* surface =
-		IMG_Load("../Demos/SpriteTest/SpriteTest/Graphics/Textures/Magnemite_small.png");
-	sprite.texture = SDL_CreateTextureFromSurface(engine->SDLRenderer, surface);
-	sprite.pixels.width = surface->w;
-	sprite.pixels.height = surface->h;
-	SDL_FreeSurface(surface);
-	cameraEntity.components.insert(Pair<uint8_t, uint64_t>(CAMERA2D, (uint64_t)&camera));
-	magnemite.components.insert(Pair<uint8_t, uint64_t>(SPRITE, (uint64_t)&sprite));
-	camera.container = &cameraEntity;
-	sprite.container = &magnemite;
-	scene2D.cameras.push_back(camera);
-	scene2D.sprites.push_back(sprite);
 	scene2D.entities.push_back(magnemite);
+
+	Sprite magnemiteSprite;
+	SDL_Surface* magnemiteSurface =
+		IMG_Load("../Demos/SpriteTest/SpriteTest/Graphics/Textures/Magnemite_small.png");
+	magnemiteSprite.texture = SDL_CreateTextureFromSurface(engine->SDLRenderer, magnemiteSurface);
+	magnemiteSprite.pixels.width = magnemiteSurface->w;
+	magnemiteSprite.pixels.height = magnemiteSurface->h;
+	SDL_FreeSurface(magnemiteSurface);
+	magnemiteSprite.entityID = scene2D.entities.size() - 1;
+	scene2D.entities.back().attachedComponentsFlag |= SPRITE;
+	scene2D.activeSprites.push_back(magnemiteSprite);
+
+	Entity2D apple;
+	//As an attached entity, these are relative to magnemite
+	apple.transform.position_px.x = 0;
+	apple.transform.position_px.y = -100;
+	//Attach apple to magnemite
+	apple.parent = scene2D.entities.size() - 1;
+	scene2D.entities.back().children.push_back(scene2D.entities.size());
+	//Add to scene.
+	scene2D.entities.push_back(apple);
+
+	Sprite appleSprite;
+	SDL_Surface* appleSurface =
+		IMG_Load("../Demos/SpriteTest/SpriteTest/Graphics/Textures/apple.png");
+	appleSprite.texture = SDL_CreateTextureFromSurface(engine->SDLRenderer, appleSurface);
+	appleSprite.pixels.width = appleSurface->w;
+	appleSprite.pixels.height = appleSurface->h;
+	SDL_FreeSurface(appleSurface);
+	appleSprite.entityID = scene2D.entities.size() - 1;
+	scene2D.entities.back().attachedComponentsFlag |= SPRITE;
+	scene2D.activeSprites.push_back(appleSprite);
+
 	application->scene.subscenes2D.push_back(scene2D);
+
+	////////////
 
 	do
 	{
-		magnemite.transform.rotation_rad += (M_PI / 6) * 1 / (engine->appConfig.targetFPS);
 		time->frame.setStart();
-		time->cycle.setStart();
+		time->process.setStart();
 
-		time->lag += time->frame.delta_ns / MS_PER_NS;
+		time->lag_ms += time->frame.delta_ns / MS_PER_NS;
 
 		OS::listenForEvents(engine);
 		Lania::script(engine, application);
 		Lania::compute(engine, application);
 		Lania::output(engine, application);
 
-		time->cycle.setEnd();
+		time->process.setEnd();
 		
 		//0 or less for unlimited FPS use.
 		if (engine->appConfig.targetFPS > 0)
 		{
 			int delay = (1000.0 / engine->appConfig.targetFPS) -
-				(engine->timer.cycle.delta_ns / MS_PER_NS);
+				(engine->timer.process.delta_ns / MS_PER_NS);
 			if (delay > 0)
 				SDL_Delay(delay);
 		}
@@ -185,6 +231,7 @@ void Lania::loop(Engine* engine, Application* application)
 		passedFrames++;
 		time->FPS.setEnd();
 #ifdef _DEBUG
+		//Display FPS and other data to Window title.
 		if (time->FPS.delta_ns / MS_PER_NS >= MS_PER_S)
 		{
 			engine->FPS = (passedFrames / (time->FPS.delta_ns / S_PER_NS));
@@ -195,13 +242,13 @@ void Lania::loop(Engine* engine, Application* application)
 			switch (engine->renderer)
 			{
 				case SDL_RENDERER: rendererString = "SDL"; break;
-				case LANIA_OPENGL_RENDERER: rendererString = "Lania"; break;
+				case LANIA_OPENGL_RENDERER:
 				case LANIA_VULKAN_RENDERER: rendererString = "Lania"; break;
 			}
 
 			String FPSString = std::to_string(engine->FPS);
 			String frameUtilizationString = 
-				std::to_string((int)(((double)time->cycle.delta_ns / (double)time->frame.delta_ns) * 100));
+				std::to_string((int)(((double)time->process.delta_ns / (double)time->frame.delta_ns) * 100));
 			String batteryString = std::to_string(engine->platform.batteryLife_pct);
 			SDL_SetWindowTitle(engine->window,
 				(engine->appConfig.appName + " - Lania Engine Debug ->" + 
@@ -227,10 +274,16 @@ void Lania::compute(Engine* engine, Application* application)
 {
 	Timer* time = &engine->timer;
 	time->compute.setStart();
-	while (time->lag >= MS_PER_UPDATE)
+
+	while (time->lag_ms >= MS_PER_UPDATE)
 	{
-		time->simulation += MS_PER_UPDATE;
-		time->lag -= MS_PER_UPDATE;
+		//Development test
+		//Rotate magnemite through List reference. Pointers dangle after auto-reallocation.
+		application->scene.subscenes2D.at(0).entities.at(1).transform.rotation_rad += 
+			(M_PI / 6) * 1 / (engine->appConfig.targetFPS);
+
+		time->simulation_ms += MS_PER_UPDATE;
+		time->lag_ms -= MS_PER_UPDATE;
 	}
 	time->compute.setEnd();
 }
