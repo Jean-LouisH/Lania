244bc1613fd1cff3fcc54de2e47e0869aefab7c1
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-07-25 22:55:02 -0400

    Laid out the assignment of the renderable more clearly. Added support for sprite scaling.

--- LaniaEngine/Rendering/RendererSDL.cpp
+++ LaniaEngine/Rendering/RendererSDL.cpp
@@ -33,6 +33,8 @@ void Lania::RendererSDL::drawSprites(
 
 			double spriteWidth = sprite->pixels.width;
 			double spriteHeight = sprite->pixels.height;
+			double spriteWidthScaled = spriteWidth * spriteTransform->scale.x;
+			double spriteHeightScaled = spriteHeight * spriteTransform->scale.y;
 			double cameraWidth = camera.viewport_px.width;
 			double cameraHeight = camera.viewport_px.height;
 			double cameraX = cameraTransform->position_px.x;
@@ -64,14 +66,18 @@ void Lania::RendererSDL::drawSprites(
 					-((spriteTransform->rotation_rad - cameraTransform->rotation_rad) * 180.0);
 			}
 
-			SDLRenderable renderable = {
-				sprite->texture,
-				{0,0, spriteWidth, spriteHeight },
-				{(int)((((spriteX - spriteWidth / 2 - cameraX) / cameraWidth) + 0.5) * (double)windowWidth_px),
-				(int)((((cameraY - spriteY - spriteHeight / 2) / cameraHeight) + 0.5) * (double)windowHeight_px),
-				(int)(spriteWidth / cameraWidth * (double)windowWidth_px),
-				(int)(spriteHeight / cameraHeight * (double)windowHeight_px)}
-			};
+			SDLRenderable renderable;
+			renderable.texture = sprite->texture;
+
+			renderable.textureRect.x = 0;
+			renderable.textureRect.y = 0;
+			renderable.textureRect.w = spriteWidth;
+			renderable.textureRect.h = spriteHeight;
+
+			renderable.renderingRect.x = (int)((((spriteX - spriteWidth / 2 - cameraX) / cameraWidth) + 0.5) * (double)windowWidth_px);
+			renderable.renderingRect.y = (int)((((cameraY - spriteY - spriteHeight / 2) / cameraHeight) + 0.5) * (double)windowHeight_px);
+			renderable.renderingRect.w = (int)(spriteWidthScaled / cameraWidth * (double)windowWidth_px);
+			renderable.renderingRect.h = (int)(spriteHeightScaled / cameraHeight * (double)windowHeight_px);
 
 			SDL_RenderCopyEx(
 				SDLRenderer,
