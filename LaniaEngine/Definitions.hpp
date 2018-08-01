62f8ec3192fb9c75f44fdd82536c010d7f888cbb
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-07-31 23:34:43 -0400

    Added definitions to contain type aliases

---
+++ LaniaEngine/Definitions.hpp
@@ -0,0 +1,15 @@
+/**
+*                      This file is part of:
+*                          Lania Engine
+*           https://jean-louish.github.io/LaniaEngine/
+*
+* Definitions
+*
+* Contains type aliases used throughout the engine.
+*
+* Copyright (c) 2017-2018 Jean-Louis Haywood. All rights reserved.
+*/
+
+using EntityID = uint64_t;
+using LayerType = uint8_t;
+using ComponentListIndex = uint64_t;
