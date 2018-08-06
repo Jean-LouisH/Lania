3acea8edb195667c47618f8e9e8eb17f9ef9c579
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-08-06 18:06:02 -0400

    Added point lock 2D for compute operations to fix entities in place, despite physics.

---
+++ LaniaEngine/Application/Scenes/2D/PointLock2D.hpp
@@ -0,0 +1,25 @@
+/**
+*                      This file is part of:
+*                          Lania Engine
+*           https://jean-louish.github.io/LaniaEngine/
+*
+* Point Lock 2D
+*
+* Fixes entities to stored locations despite physics operations on them.
+*
+* Copyright (c) 2017-2018 Jean-Louis Haywood. All rights reserved.
+* License: https://github.com/Jean-LouisH/LaniaEngine/blob/master/LICENSE
+*/
+
+#pragma once
+
+#include <Application/Scenes/2D/Entity2D.hpp>
+
+namespace Lania
+{
+	typedef struct PointLock2D
+	{
+		EntityID entityID;
+		Vector2 point;
+	}PointLock2D;
+}
