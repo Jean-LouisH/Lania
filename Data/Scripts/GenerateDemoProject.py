613b9e90310508abe5eccdb8db67e6e2b4a5cc2b
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-08-06 18:04:16 -0400

    Introduce development script for demo directories. Incomplete.

---
+++ Data/Scripts/GenerateDemoProject.py
@@ -0,0 +1,9 @@
+# Automatically forms a demo project directory for Lania
+# without having to copy and rename existing folders.
+
+import os.path
+
+demo_directory_path = "../../Demos/"
+
+def main():
+	
