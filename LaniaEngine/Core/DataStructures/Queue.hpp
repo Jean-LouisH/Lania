916e78213b72e2305cbd47b7cb9cd05618b848f0
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-08-12 00:09:04 -0400

    Added Queue data structure

---
+++ LaniaEngine/Core/DataStructures/Queue.hpp
@@ -0,0 +1,20 @@
+/**
+*                      This file is part of:
+*                          Lania Engine
+*           https://jean-louish.github.io/LaniaEngine/
+*
+* Queue
+*
+* Copyright (c) 2017 Jean-Louis Haywood. All rights reserved.
+* License: https://github.com/Jean-LouisH/LaniaEngine/blob/master/LICENSE
+*/
+
+#pragma once
+
+#include <queue>
+
+namespace Lania
+{
+	template <class T>
+	using Queue = std::queue<T>;
+}
