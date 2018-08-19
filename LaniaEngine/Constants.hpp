e8cfc39e0ac636e59c85fa5472a34fc5e459ce14
Jean-LouisH <Haywood.JeanLouis@gmail.com>
2018-08-19 18:33:16 -0400

    Added orders of magnitude to constants

--- LaniaEngine/Constants.hpp
+++ LaniaEngine/Constants.hpp
@@ -12,10 +12,20 @@
 */
 
 #define MS_PER_UPDATE 8
-#define S_PER_NS 1000000000
-#define MS_PER_NS 1000000
-#define MS_PER_S 1000
-#define S_PER_UPDATE ((double)MS_PER_UPDATE / (double)MS_PER_S)
+
+#define TERA 1000000000000.0
+#define GIGA 1000000000.0
+#define MEGA 1000000.0
+#define KILO 1000.0
+#define MILLI 0.001
+#define MICRO 0.000001
+#define NANO 0.000000001
+#define PICO 0.000000000001
+
+#define NS_IN_S (1.0 / NANO)
+#define NS_IN_MS (MILLI / NANO)
+#define MS_IN_S (1.0 / MILLI)
+#define S_PER_UPDATE ((double)MS_PER_UPDATE / (double)MS_IN_S)
 #define UPDATES_PER_S (1.0 / (double)S_PER_UPDATE)
 
 #define EARTH_GRAVITY 9.80665
