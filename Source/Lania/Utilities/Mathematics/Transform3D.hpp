d350225f1e7d3caaefce162836e370f9ab7f26aa
Luleivon <Louis_Ml@live.co.uk>
2018-12-18 17:08:45 -0500

    Added Transform3D

---
+++ Source/Lania/Utilities/Mathematics/Transform3D.hpp
@@ -0,0 +1,33 @@
+/**
+*                      This file is part of:
+*                          Lania
+*           https://jean-louish.github.io/Lania/
+*
+* Transform 3D
+*
+* Position, rotation and scale of entities in 3-space.
+*
+* Copyright (c) 2017-2018 Jean-Louis Haywood. All rights reserved.
+* License: https://github.com/Jean-LouisH/Lania/blob/master/LICENSE
+*/
+
+#pragma once
+
+#include "Vector3.hpp"
+
+namespace Lania
+{
+	typedef struct Transform3D
+	{
+		Vector3 position_m;
+		Vector3 rotation_rad;
+		Vector3 scale;
+
+		Transform3D()
+		{
+			this->scale.x = 1;
+			this->scale.y = 1;
+			this->scale.z = 1;
+		}
+	}Transform3D;
+}
