8ac082fa6df8ac3ad6052a4311945dbeb552be99
Luleivon <Louis_Ml@live.co.uk>
2018-12-18 17:06:37 -0500

    Added file for 2D rendering

---
+++ Source/Lania/Engines/Rendering/Rendering2D.hpp
@@ -0,0 +1,23 @@
+/**
+*                      This file is part of:
+*                          Lania
+*           https://jean-louish.github.io/Lania/
+*
+* Rendering2D
+*
+* Rendering sub engine for 2D graphics.
+*
+* Copyright (c) 2017-2018 Jean-Louis Haywood. All rights reserved.
+* License: https://github.com/Jean-LouisH/Lania/blob/master/LICENSE
+*/
+
+#pragma once
+
+
+namespace Lania
+{
+	namespace Rendering2D
+	{
+
+	}
+}
