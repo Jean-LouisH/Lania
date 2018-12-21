b893d0bb306f18b30e7f79011c1dc67035c531ed
Luleivon <Louis_Ml@live.co.uk>
2018-12-21 12:15:04 -0400

    Defined constant point force 2D components

---
+++ Source/Lania/Application/Scene/2D/ConstantPointForce2D.hpp
@@ -0,0 +1,29 @@
+/**
+*                      This file is part of:
+*                          Lania
+*           https://jean-louish.github.io/Lania/
+*
+* Constant Point Force 2D
+*
+* A component that applies a constant scalar attraction or repulsion force from a point.
+*
+* Copyright (c) 2017-2018 Jean-Louis Haywood. All rights reserved.
+* License: https://github.com/Jean-LouisH/Lania/blob/master/LICENSE
+*/
+
+#pragma once
+
+#include "Entity2D.hpp"
+
+namespace Lania
+{
+	typedef struct ConstantPointForce2D
+	{
+		EntityID entityID;
+		double force_N;
+
+		ConstantPointForce2D() :
+			entityID(NO_PARENT)
+		{}
+	}ConstantPointForce2D;
+}
