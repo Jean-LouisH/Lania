26bdde4094d39b0c965dd8e27aee6e1865e4b78c
Jean-LouisH <LouisLou512.e@gmail.com>
2019-07-27 01:09:57 -0400

    Placed all data structure implementation on standard library

--- Source/Lania/Utilities/DataStructures/String.hpp
+++ Source/Lania/Utilities/DataStructures/String.hpp
@@ -16,31 +16,4 @@
 namespace Lania
 {
 	using String = std::string;
-
-	//typedef struct String
-	//{
-	//	char* data;
-	//	int length;
-	//	int capacity;
-
-	//	void clear();
-	//	void appendChar(const char character);
-	//	void appendCharArray(const char* charArray);
-	//	void appendString(const String string);
-
-	//	void operator+=(const char character)
-	//	{
-	//		appendChar(character);
-	//	}
-
-	//	void operator+=(const char* charArray)
-	//	{
-	//		appendCharArray(charArray);
-	//	}
-
-	//	void operator+=(const String string)
-	//	{
-	//		appendString(string);
-	//	}
-	//}String;
 }
