d989f6e957bc67375dd98580c5537149d47c46a4
Jean-LouisH <LouisLou512.e@gmail.com>
2019-07-27 00:19:47 -0400

    Defined font class initially

---
+++ Source/Lania/Application/Scene/Assets/Font.hpp
@@ -0,0 +1,21 @@
+/**
+*                      This file is part of:
+*                          Lania
+*           https://jean-louish.github.io/Lania/
+*
+* Font
+*
+* Copyright (c) 2017-2019 Jean-Louis Haywood. All rights reserved.
+* License: https://github.com/Jean-LouisH/Lania/blob/master/LICENSE
+*/
+
+#pragma once
+
+namespace Lania
+{
+	class Font
+	{
+	private:
+	public:
+	};
+}
