d6acad9c0faffaa8f48e92ddbd4a513ac89abc57
Jean-LouisH <LouisLou512.e@gmail.com>
2019-07-28 14:07:53 -0400

    Updated default shaders for handling textures

--- Source/Lania/Engines/Rendering/DefaultShaders.hpp
+++ Source/Lania/Engines/Rendering/DefaultShaders.hpp
@@ -21,19 +21,30 @@ namespace Lania
 		const char defaultVertexShader[] =
 
 			"#version 330 core\n"
-			"layout(location = 0) in vec3 aPos;\n"
+			"layout (location = 0) in vec3 aPos;\n"
+			"layout (location = 1) in vec3 aColour;\n"
+			"layout (location = 2) in vec2 aTexCoord;\n"
+			"out vec3 ourColour;\n"
+			"out vec3 texCoord;\n"
+
 			"void main()"
 			"{"
-			"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);"
+			"    gl_Position = vec4(aPos, 1.0);"
+			"    ourColour = aColour;"
+			"    texCoord = aTexCoord;"
 			"}";
 
 		const char defaultFragmentShader[] =
 
 			"#version 330 core\n"
 			"out vec4 FragColor;\n"
+			"in vec3 ourColour;\n"
+			"in vec2 texCoord;\n"
+			"uniform sampler2D ourTexture;\n"
+
 			"void main()"
 			"{"
-			"FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);"
+			"    FragColor = texture(ourTexture, texCoord);"
 			"}";
 	}
 }
