4d93221fafe16326ff883b6cbe80d6c4f6dc0c87
Jean-LouisH <LouisLou512.e@gmail.com>
2019-08-02 10:54:21 -0400

    Corrected shader creation on variable declaration, corrected use of shader counts in glAttachShader() and glDeleteShader() loops.

--- Source/Lania/Engines/Rendering/GLSLShader.cpp
+++ Source/Lania/Engines/Rendering/GLSLShader.cpp
@@ -17,9 +17,8 @@ void Lania::Rendering::OpenGL::GLSLShaders::use()
 
 bool Lania::Rendering::OpenGL::GLSLShaders::compileVertexShaders()
 {
-	GLuint vertexShader;
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	GLchar* vertexShaderSource = (GLchar*)Rendering::OpenGL::defaultVertexShader;
-	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 	this->vertexShaders.push_back(vertexShader);
@@ -28,9 +27,8 @@ bool Lania::Rendering::OpenGL::GLSLShaders::compileVertexShaders()
 
 bool Lania::Rendering::OpenGL::GLSLShaders::compileFragmentShaders()
 {
-	GLuint fragmentShader;
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	GLchar* fragmentShaderSource = (GLchar*)Rendering::OpenGL::defaultFragmentShader;
-	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 	this->fragmentShaders.push_back(fragmentShader);
@@ -41,11 +39,11 @@ void Lania::Rendering::OpenGL::GLSLShaders::linkShaderProgram()
 {
 	GLuint shaderProgram = glCreateProgram();
 	int vertexShaderCount = this->vertexShaders.size();
-	for (int i = 0; i < this->vertexShaders.size(); i++)
+	for (int i = 0; i < vertexShaderCount; i++)
 		glAttachShader(shaderProgram, this->vertexShaders.at(i));
 
 	int fragmentShaderCount = this->fragmentShaders.size();
-	for (int i = 0; i < this->fragmentShaders.size(); i++)
+	for (int i = 0; i < fragmentShaderCount; i++)
 		glAttachShader(shaderProgram, this->fragmentShaders.at(i));
 
 	glLinkProgram(shaderProgram);
@@ -75,12 +73,12 @@ bool Lania::Rendering::OpenGL::GLSLShaders::checkCompileTimeErrors(GLuint ID, GL
 void Lania::Rendering::OpenGL::GLSLShaders::deleteShaders()
 {
 	int vertexShaderCount = this->vertexShaders.size();
-	for (int i = 0; i < this->vertexShaders.size(); i++)
+	for (int i = 0; i < vertexShaderCount; i++)
 		glDeleteShader(this->vertexShaders.at(i));
 	this->vertexShaders.clear();
 
 	int fragmentShaderCount = this->fragmentShaders.size();
-	for (int i = 0; i < this->fragmentShaders.size(); i++)
+	for (int i = 0; i < fragmentShaderCount; i++)
 		glDeleteShader(this->fragmentShaders.at(i));
 	this->fragmentShaders.clear();
 }
