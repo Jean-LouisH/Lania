455dd7862847c84f2fedb0e0f281a13a88c1ab36
Jean-LouisH <LouisLou512.e@gmail.com>
2019-09-06 23:58:43 -0400

    Suspended forced high performance graphics in favour of system defined setting

--- Source/Lania/Core/HAL/GPU.hpp
+++ Source/Lania/Core/HAL/GPU.hpp
@@ -13,14 +13,14 @@
 
 #pragma once
 
-#ifdef _WIN32
-
-#include <windows.h>
-// Use discrete GPU by default.
-
-extern "C" 
-{
-	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
-	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
-}
-#endif
+//#ifdef _WIN32
+//
+//#include <windows.h>
+//// Use discrete GPU by default.
+//
+//extern "C" 
+//{
+//	__declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+//	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+//}
+//#endif
