7cd79d411e97d7820a199e9d1892c76046503ead
Jean-LouisH <LouisLou512.e@gmail.com>
2019-10-11 19:36:21 -0400

    Defined a header for editor icon image data

---
+++ Source/Lania/Editor/EditorIcon.hpp
@@ -0,0 +1,20 @@
+/**
+*                      This file is part of:
+*                          Lania
+*           https://jean-louish.github.io/Lania/
+*
+* Editor Icon
+*
+* Copyright (c) 2017-2019 Jean-Louis Haywood. All rights reserved.
+* License: https://github.com/Jean-LouisH/Lania/blob/master/LICENSE
+*/
+
+#pragma once
+
+namespace Lania
+{
+	namespace Editor
+	{
+		const char iconImageData[] = R"()";
+	}
+}
