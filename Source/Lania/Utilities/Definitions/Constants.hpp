d043c9399d7fea54d4273cf2eb32afd2bfc4f651
Jean-LouisH <LouisLou512.e@gmail.com>
2019-12-28 20:53:01 -0500

    Corrected Debug path to Default Debug to avoid gitignore of the demo directory

--- Source/Lania/Utilities/Definitions/Constants.hpp
+++ Source/Lania/Utilities/Definitions/Constants.hpp
@@ -38,7 +38,7 @@ namespace Lania
 
 #define NO_ENTITY 0 
 
-#define DEFAULT_DEBUG_EXPORT_PATH "../../Demos/Debug/Lania/"
+#define DEFAULT_DEBUG_EXPORT_PATH "../../Demos/Default Debug/Lania/"
 #define EDITOR_APPLICATION_NAME "Lania Editor"
 
 	enum LayerTypes
